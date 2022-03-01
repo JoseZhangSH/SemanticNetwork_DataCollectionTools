dfcb00cf5b3bef9da9c0852d41e13136cf65cd5d
JoseZhangSH <josezhangsh@gmail.com>
2022-03-01 23:36:57 +0800

    Update: vue config for path
    Update: vue config for path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/SemanticNetwork_DataCollectionTools/'
-    : '/'
+  publicPath: '/SemanticNetwork_DataCollectionTools/',
 }
